421fca0484457f89df40ec46a70933ce3bb51353
Pierre Turpin <pierre.turpin54@gmail.com>
2018-02-26 23:41:59 +0100

    Implement basic detection of the line number of the lambda declaration

--- src/main/java/fr/pturpin/lambdaString/transform/LambdaMetaInfo.java
+++ src/main/java/fr/pturpin/lambdaString/transform/LambdaMetaInfo.java
@@ -1,5 +1,12 @@
 package fr.pturpin.lambdaString.transform;
 
+import org.objectweb.asm.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.OptionalInt;
+import java.util.function.IntConsumer;
+
 import static java.util.Objects.requireNonNull;
 
 public final class LambdaMetaInfo {
@@ -36,6 +43,71 @@ public final class LambdaMetaInfo {
 
     public String getMethodDesc() {
         return methodDesc;
+    }
+
+    public OptionalInt getDeclarationLine() {
+        int line = 0;
+        try {
+            line = computeDeclarationLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line == -1 ? OptionalInt.empty() : OptionalInt.of(line);
+    }
+
+    private int computeDeclarationLine() throws IOException {
+        int[] line = new int[]{ -1 };
+        InputStream classStream = getClass().getClassLoader().getResourceAsStream(declaringClassName + ".class");
+        ClassReader cr = new ClassReader(classStream);
+        FetchingLineNumberOfIndyClassVisitor visitor = new FetchingLineNumberOfIndyClassVisitor(
+                Opcodes.ASM5,
+                methodName,
+                methodDesc,
+                foundLine -> line[0] = foundLine);
+        cr.accept(visitor, 0);
+        return line[0];
+    }
+
+    private static final class FetchingLineNumberOfIndyClassVisitor extends ClassVisitor {
+
+        private final String methodName;
+        private final String methodDesc;
+        private final IntConsumer onLine;
+
+        FetchingLineNumberOfIndyClassVisitor(int api, String methodName, String methodDesc, IntConsumer onLine) {
+            super(api);
+            this.methodName = requireNonNull(methodName);
+            this.methodDesc = requireNonNull(methodDesc);
+            this.onLine = requireNonNull(onLine);
+        }
+
+        @Override
+        public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+            if (methodName.equals(name) && methodDesc.equals(desc)) {
+                return new FetchingLineNumberOfIndyMethodVisitor(api, onLine);
+            }
+            return null;
+        }
+    }
+
+    private static final class FetchingLineNumberOfIndyMethodVisitor extends MethodVisitor {
+
+        private final IntConsumer onLine;
+        private boolean lineFound;
+
+        FetchingLineNumberOfIndyMethodVisitor(int api, IntConsumer onLine) {
+            super(api);
+            this.onLine = requireNonNull(onLine);
+        }
+
+        @Override
+        public void visitLineNumber(int line, Label start) {
+            if (!lineFound) {
+                onLine.accept(line);
+                lineFound = true;
+            }
+        }
+
     }
 
 }
